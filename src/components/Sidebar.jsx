57dc22d86caa5dd052752b1fd35827dc75b72040
OluwadahunsiA <tommydate0@gmail.com>
2022-05-23 23:04:34 +0300

    edit sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,58 +19,50 @@ const Sidebar = () => {
     }
   };
   return (
-    <div>
-      <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
-        {activeMenu && (
-          <>
-            {' '}
-            <div className='flex justify-between items-center'>
-              <Link
-                to='/'
-                onClick={handleCloseSideBar}
-                className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
-              >
-                <SiShopware /> <span>Shoppy</span>
-              </Link>
+    activeMenu && (
+      <div className='relative ml-3 mr-5 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+        {' '}
+        <div className='flex justify-between items-center'>
+          <Link
+            to='/'
+            onClick={handleCloseSideBar}
+            className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
+          >
+            <SiShopware /> <span>Shoppy</span>
+          </Link>
 
-              <TooltipComponent content='Menu' position='BottomCenter'>
-                <button
-                  type='button'
-                  onClick={() =>
-                    setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+          <TooltipComponent content='Menu' position='BottomCenter'>
+            <button
+              type='button'
+              onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+              className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
+            >
+              <MdOutlineCancel />
+            </button>
+          </TooltipComponent>
+        </div>
+        <div className='mt-10'>
+          {links.map((item) => (
+            <div key={item.title}>
+              <p className='text-gray-400 m-3 mt-4 uppercase'>{item.title}</p>
+              {item.links.map((Link) => (
+                <NavLink
+                  to={`/${Link.name}`}
+                  key={Link.name}
+                  onClick={handleCloseSideBar}
+                  className={({ isActive }) =>
+                    isActive ? activeLink : normalLink
                   }
-                  className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
                 >
-                  <MdOutlineCancel />
-                </button>
-              </TooltipComponent>
-            </div>
-            <div className='mt-10'>
-              {links.map((item) => (
-                <div key={item.title}>
-                  <p className='text-gray-400 m-3 mt-4 uppercase'>
-                    {item.title}
-                  </p>
-                  {item.links.map((Link) => (
-                    <NavLink
-                      to={`/${Link.name}`}
-                      key={Link.name}
-                      onClick={handleCloseSideBar}
-                      className={({ isActive }) =>
-                        isActive ? activeLink : normalLink
-                      }
-                    >
-                      {Link.icon}
-                      <span className='capitalize'>{Link.name}</span>
-                    </NavLink>
-                  ))}
-                </div>
+                  {Link.icon}
+                  <span className='capitalize'>{Link.name}</span>
+                </NavLink>
               ))}
             </div>
-          </>
-        )}
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
