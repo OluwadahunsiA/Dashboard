29ddde790b0873e4f4b0b7e765aec44758c320a2
OluwadahunsiA <tommydate0@gmail.com>
2022-05-25 21:23:00 +0300

    add Employees and Orders

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Inject,
+  Search,
+} from '@syncfusion/ej2-react-grids';
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>
-      <div></div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Employees' />
+      <GridComponent
+        id='gridcomp'
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page]} />
+      </GridComponent>
     </div>
   );
 };
